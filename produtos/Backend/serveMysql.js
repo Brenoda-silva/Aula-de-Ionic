1762b768475c5a1c27fc6adfde921ecfeaa40203
Júlio Monteiro <nobresilva360@gmail.com>
2026-06-16 21:42:25 -0300

    .
    So não esta funcionando o put

--- produtos/Backend/serveMysql.js
+++ produtos/Backend/serveMysql.js
@@ -35,7 +35,6 @@ app.get('/produtos', (req, res) => {
 
 app.post('/produtos', (req, res) => {
     const novoProduto = {
-        id: Date.now(),
         ...req.body
     };
 
@@ -51,6 +50,40 @@ app.post('/produtos', (req, res) => {
         });
     });
 });
+
+app.put('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    const atualizarProduto = req.body;
+
+    db.query(
+        'UPDATE produtos SET ? WHERE id = ?',
+        [atualizarProduto, id],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    erro: err.message
+                });
+            }
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({
+                    erro: 'Produto não encontrado'
+                });
+            }
+
+            res.json({
+                id,
+                ...atualizarProduto
+            });
+        }
+    );
+});
+app.delete('/produtos/:id', (req,res) => {
+    db.query('DELETE FROM produtos WHERE id = ?',
+        [req.params.id],
+        () => res.sendStatus(200)
+    );
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
