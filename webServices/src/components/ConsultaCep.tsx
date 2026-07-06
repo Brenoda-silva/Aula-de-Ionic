fbf86972bb1a37e3a81e2b45e37d2bf36a52ede4
Breno <nobresilva360@gmail.com>
2026-07-06 20:09:53 -0300

    .

---
+++ webServices/src/components/ConsultaCep.tsx
@@ -0,0 +1,52 @@
+import { IonContent, IonPage, IonInput, IonButton, IonList, IonItem, IonLabel } from '@ionic/react';
+import {  useState } from 'react';
+// import './Home.css';
+
+
+const ConsultaCep: React.FC = () => {
+
+    const [localidade, setLocalidade] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cep, setCep] = useState("");
+
+
+    async function handleConsultarCep() {
+
+        try {
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
+        const data = await resposta.json();
+
+        setLocalidade(data.localidade)
+        setEstado(data.estado)
+        setCep(data.cep)
+        console.log(data)
+    } catch (erro) {
+        console.error("Cep Invalido")
+    }
+    }
+
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+                <label>CEP</label>
+                <IonInput aria-label="CEP" value={cep} onChange={(e) => setCep((e.target as HTMLInputElement).value)}></IonInput>
+                <IonButton onClick={handleConsultarCep}>Consultar</IonButton>
+
+                <IonList>
+                    <IonItem>
+                        <IonLabel>CEP: {cep}</IonLabel>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel>Cidade: {localidade}</IonLabel>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel>Estado: {estado}</IonLabel>
+                    </IonItem>
+                </IonList>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default ConsultaCep;
--- webServices/src/pages/Home.tsx
+++ webServices/src/pages/Home.tsx
@@ -1,44 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 
-import { useEffect, useState } from 'react';
+import ConsultaCep from '../components/ConsultaCep';
 
 const Home: React.FC = () => {
 
-  const [cidade, setCidade] = useState("");
-  const [estado, setEstado] = useState("")
-  const [regiao, setRegiao] = useState("")
-
-  useEffect(() => {
-
-    fetch("https://viacep.com.br/ws/48680000/json/")
-      .then(res => res.json())
-      .then(data => {
-        setCidade(data.localidade)
-        setEstado(data.estado)
-        setRegiao(data.regiao)
-        console.log(data)
-      });
-  }, []);
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        
         <div>
-          <h1>Consultar CEP</h1>
-          <h2>Cidade</h2>
-          <p>{cidade}</p>
-          <p>{estado}</p>
-          <p>{regiao}</p>
+          <ConsultaCep />
         </div>
       </IonContent>
     </IonPage>
